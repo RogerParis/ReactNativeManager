4a5d048070303ff52fa91a7aa357977bf9857520
RogerParis <roger.parisvicent@gmail.com>
2017-02-04 17:24:45 +0100

    adds list item and auto refresh

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 import { Actions } from 'react-native-router-flux';
 
@@ -15,12 +16,23 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
 
-  return () => {
+  return (dispatch) => {
     firebase.database().ref(`users/${currentUser.uid}/employees`)
       .push({ name, phone, shift })
       .then(() => {
         dispatch({ type: EMPLOYEE_CREATE });
         Actions.employeeList({ type: 'reset' })
       });
+  };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => { 
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .on('value', snapshot => {
+      dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() })
+    })
   };
 };
